cc2f3cc5872e1f95ceb9326c885eddf3810fadff
Martin <m@m.m>
2024-09-10 19:53:52 +0200

    shell setup

--- scripts/shell-setup.sh
+++ scripts/shell-setup.sh
@@ -1,4 +1,4 @@
 apt install -y zsh
 apt install -y nnn
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
