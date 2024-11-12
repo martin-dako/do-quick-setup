7e8928c4da7a4dda127021a6a2bcc3fab3c26836
Martin <m@m.m>
2024-11-12 19:22:10 +0100

    name of install.sh script changed to install-<reponame>.sh

--- scripts/spring-boot-21.sh
+++ scripts/spring-boot-21.sh
@@ -126,3 +126,4 @@ sudo chmod +x /var/install-$REPO_NAME_PATH.sh
 # Notify the user
 echo "The script has been created at /var/install-<reponame>.sh and can be run by executing ./install-<reponame>.sh"
 sudo /var/install-$REPO_NAME_PATH.sh
+echo "script is now executed"
